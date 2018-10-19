6a8ef5eae7839ac67d28aded379b96b7da47a4fb
Joao Vitor Gazola <joao.v.gazola@gmail.com>
2018-10-19 11:03:06 -0400

    Revert "Botão clientes"
    This reverts commit 819a96c0840636180bd715a0c020866cbff1ac14.

--- crud2/resources/views/layouts/app.blade.php
+++ crud2/resources/views/layouts/app.blade.php
@@ -26,7 +26,6 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <a class="text-black-50" href="{{url('clientes')}}">Clientes</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -54,6 +53,7 @@
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
+
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
@@ -66,7 +66,6 @@
                                     </form>
                                 </div>
                             </li>
-
                         @endguest
                     </ul>
                 </div>
